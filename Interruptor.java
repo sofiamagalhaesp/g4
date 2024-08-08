1970313f3bb747d4f9958b3c964abee52477a5ed
saulocrn <95320479+saulocrn@users.noreply.github.com>
2024-08-08 18:26:33 -0300

    Create Interruptor.java

---
+++ Interruptor.java
@@ -0,0 +1,9 @@
+public class Interruptor extends Componente {
+
+    public Interruptor(String nome, int quantidade) {
+
+        super(nome, quantidade, 0.0); // passa 0.0 como potência
+
+    }
+
+}
